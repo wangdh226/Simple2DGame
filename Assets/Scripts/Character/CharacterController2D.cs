388d039cedab2ec3b6ebe2db9ac1c38e8ad76ddc
David H Wang <wang.dh226@gmail.com>
2022-07-16 19:40:36 -0400

    Update NPMC for jumping, crouching, ceilingcheck - still working on jump+ceilcheck

--- Assets/Scripts/Character/CharacterController2D.cs
+++ Assets/Scripts/Character/CharacterController2D.cs
@@ -40,20 +40,20 @@ public class CharacterController2D : MonoBehaviour {
     }
 
     private void FixedUpdate() {
-        bool wasGrounded = m_Grounded;
-        m_Grounded = false;
+        //    bool wasGrounded = m_Grounded;
+        //    m_Grounded = false;
 
-        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
-        foreach (Collider2D collider in colliders) {
-            if (collider.gameObject != gameObject) {
-                m_Grounded = true;
-                if (!wasGrounded) {
-                    OnLandEvent.Invoke();
-                }
-            }
-        }
+        //    // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+        //    // This can be done using layers instead but Sample Assets will not overwrite your project settings.
+        //    Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+        //    foreach (Collider2D collider in colliders) {
+        //        if (collider.gameObject != gameObject) {
+        //            m_Grounded = true;
+        //            if (!wasGrounded) {
+        //                OnLandEvent.Invoke();
+        //            }
+        //        }
+        //    }
     }
 
 
@@ -98,28 +98,28 @@ public class CharacterController2D : MonoBehaviour {
             m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
 
             // If the input is moving the player right and the player is facing left...
-            if (move > 0 && !m_FacingRight) {
-                Flip();
-            } else if (move < 0 && m_FacingRight) {
-                Flip();
-            }
+            //if (move > 0 && !m_FacingRight) {
+            //    Flip();
+            //} else if (move < 0 && m_FacingRight) {
+            //    Flip();
+            //}
         }
         // If the player should jump...
-        if (m_Grounded && jump) {
-            // Add a vertical force to the player.
-            m_Grounded = false;
-            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
-        }
+        //if (m_Grounded && jump) {
+        //    // Add a vertical force to the player.
+        //    m_Grounded = false;
+        //    m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+        //}
     }
 
 
-    private void Flip() {
-        // Switch the way the player is labelled as facing.
-        m_FacingRight = !m_FacingRight;
+    //private void Flip() {
+    //    // Switch the way the player is labelled as facing.
+    //    m_FacingRight = !m_FacingRight;
 
-        // Multiply the player's x local scale by -1.
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-    }
+    //    // Multiply the player's x local scale by -1.
+    //    Vector3 theScale = transform.localScale;
+    //    theScale.x *= -1;
+    //    transform.localScale = theScale;
+    //}
 }
--- Assets/Scripts/Character/NewPlayerMovementController.cs
+++ Assets/Scripts/Character/NewPlayerMovementController.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewPlayerMovementController : MonoBehaviour
-{
+public class NewPlayerMovementController : MonoBehaviour {
     // References to update in Inspector
     [SerializeField] private Transform groundCheck;
     [SerializeField] private Transform ceilingCheck;
@@ -25,22 +24,25 @@ public class NewPlayerMovementController : MonoBehaviour
     private float horizontalSpeed_target = 0f;
     private float verticalSpeed = 0f;
     private float verticalSpeed_target = 0f;
-    
+
 
     // Private stateful bools
     private bool isJumping = false;     // Player press Space to send sprite into air
     private bool isFalling = false;     // Player is in air without pressing Space
     private bool isCrouching = false;   // Player press ctrl to make sprite crouch
+    private bool wasCrouching = false;  // Store for previous Crouch state
     private bool isGrounded = false;    // Player is on designated 'ground'
+    private bool wasGrounded = false;   // Store for previous Grounded state
     private bool isFacingRight = true;  // Player is facing right - left if false
-    
+
     // Constants
-    private const float GROUNDED_RADIUS = .2f;
-    private Vector2 ZERO_VELOCITY = Vector2.zero;
+    private const float GROUNDED_RADIUS = 0.2f;
+    private const float CEILING_RADIUS = 0.2f;
     private const float CROUCH_RUN_MULTIPLIER = 0.33f;
     private const float CROUCH_JUMP_MULTIPLIER = 0.8f;
     private const float AIR_SPEED_MULTIPLIER = 0.8f;
     private const float MOVEMENT_SMOOTHING_FACTOR = .05f;
+    private Vector2 ZERO_VELOCITY = Vector2.zero;
 
     private void OnValidate() {
         if (animator == null) {
@@ -57,70 +59,91 @@ public class NewPlayerMovementController : MonoBehaviour
         }
     }
 
+    /** Method to update player states - is* bools
+     *  I think this method runs *more often* than FixedUpdate, so use as continuous update for state
+     */
     void Update() {
-
-        // Constantly update the velocities and send to Animator
-        horizontalSpeed = playerRigidbody2D.velocity.x;
-        verticalSpeed = playerRigidbody2D.velocity.y;
-        animator.SetFloat("Horizontal_Speed", Mathf.Abs(horizontalSpeed));
-        animator.SetFloat("Vertical_Speed", verticalSpeed);
 
         // Set horizontalSpeed_target for smoothdamp
         horizontalSpeed_target = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        // Check for jump input
+        // Check for jump input and set verticalSpeed_target
         if (Input.GetButtonDown("Jump") && isGrounded) {
             isJumping = true;
-            animator.SetBool("IsJumping", isJumping);
-            verticalSpeed_target = jumpSpeed * playerRigidbody2D.gravityScale;
-        } 
-
-        // Check for crouch input
-        if (Input.GetButtonDown("Crouch")) {
-            isCrouching = true;
-            animator.SetBool("IsCrouching", isCrouching);
-        } else if (Input.GetButtonUp("Crouch")) {
-            isCrouching = false;
-            animator.SetBool("IsCrouching", isCrouching);
+            
         }
-    }
 
-    void FixedUpdate() {
+        // Check for crouch input and set state
+        if (Input.GetButton("Crouch") && !isJumping) {
+            isCrouching = true;
+            wasCrouching = true;
+        } else if (!Input.GetButton("Crouch") && wasCrouching) {
+            isCrouching = false;
+        }
 
-        bool wasGrounded = isGrounded;
+        //If uncrouching, check if ceiling is there.
+        if (wasCrouching && !isCrouching) {
+            if (Physics2D.OverlapCircle(ceilingCheck.position, CEILING_RADIUS, whatIsGround)) {
+                isCrouching = true;
+                isJumping = false;
+            } else {
+                isCrouching = false;
+            }
+        }
+        wasCrouching = isCrouching;
+        Debug.Log(isJumping);
+
+        // Check if player is grounded or not
         // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-        // MAKE SURE PLAYER LAYER IS NOT CHECKED!
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, GROUNDED_RADIUS, whatIsGround);
-        if(colliders.Length > 0) {
-            // If colliders exist(non player colliders)
-            foreach (Collider2D collider in colliders) {
-                if (collider.gameObject != gameObject) {
-                    // if found collider is not player collider, means contact with 'ground'
-                    // should never be player, unless WhatIsGround is not set properly
-                    isGrounded = true;
-                    if (!wasGrounded) {
-                        // if !wasGrounded in previous check => player was in air => player just landed
-                        OnLanding();
-                    }
-                }
+        // MAKE SURE PLAYER LAYER IS NOT INCLUDED IN whatIsGround!
+        wasGrounded = isGrounded;
+        if (Physics2D.OverlapCircle(groundCheck.position, GROUNDED_RADIUS, whatIsGround)) {
+            isGrounded = true;
+            if (!wasGrounded) {
+                // if !wasGrounded in previous check => player was in air => player just landed
+                OnLanding();
             }
         } else {
             // If no colliders exist => player is in the air
             OnFalling();
             isGrounded = false;
         }
+    }
 
+    /** Method to update player every frame - moving/crouching/jumping etc
+     *   Used like a frame-based discrete update based on continuous state changes from Update()
+     */
+    void FixedUpdate() {
+        // Constantly update the velocities and send to Animator
+        horizontalSpeed = playerRigidbody2D.velocity.x;
+        verticalSpeed = playerRigidbody2D.velocity.y;
+        animator.SetFloat("Horizontal_Speed", Mathf.Abs(horizontalSpeed));
+        animator.SetFloat("Vertical_Speed", verticalSpeed);
+
+        IsJumping(isJumping);
+        IsCrouching(isCrouching);
+        Move();
+    }
+
+    private void Move() {
         // Calculate new horizontal move speed and implement
         float moveSpeedX = horizontalSpeed_target * Time.fixedDeltaTime;
-        moveSpeedX *= (isCrouching ? CROUCH_RUN_MULTIPLIER : 1);
-        moveSpeedX *= (!isGrounded ? AIR_SPEED_MULTIPLIER : 1);
+        moveSpeedX *= (isCrouching ? CROUCH_RUN_MULTIPLIER : 1f);
+        moveSpeedX *= (!isGrounded ? AIR_SPEED_MULTIPLIER : 1f);
         MoveHorizontal(moveSpeedX);
 
         // Calculate new vertical move speed and implement
+        //Debug.Log(isJumping + ": " + isGrounded);
         if (isJumping && isGrounded) {
+            verticalSpeed_target = jumpSpeed * playerRigidbody2D.gravityScale;
             float moveSpeedY = verticalSpeed_target * Time.fixedDeltaTime;
-            moveSpeedY *= (isCrouching ? CROUCH_JUMP_MULTIPLIER : 1);
+            moveSpeedY *= (isCrouching ? CROUCH_JUMP_MULTIPLIER : 1f);
             MoveVertical(moveSpeedY);
+        }
+
+        // Flip player sprite if moving in opposite direction of facing
+        if ((moveSpeedX > 0 && !isFacingRight) || (moveSpeedX < 0 && isFacingRight)) {
+            Flip();
         }
     }
 
@@ -128,11 +151,6 @@ public class NewPlayerMovementController : MonoBehaviour
         // Calculate new target velocity and use SmoothDamp to accelerate to it
         Vector2 targetVelocity = new Vector2(move * 10f, playerRigidbody2D.velocity.y);
         playerRigidbody2D.velocity = Vector2.SmoothDamp(playerRigidbody2D.velocity, targetVelocity, ref ZERO_VELOCITY, MOVEMENT_SMOOTHING_FACTOR);
-
-        // Flip player sprite if moving in opposite direction of facing
-        if ((move > 0 && !isFacingRight) || (move < 0 && isFacingRight)) {
-            Flip();
-        }
     }
 
     private void MoveVertical(float move) {
@@ -140,15 +158,15 @@ public class NewPlayerMovementController : MonoBehaviour
         playerRigidbody2D.velocity = new Vector2(playerRigidbody2D.velocity.x, move * 10f);
     }
 
-    
+
     // Utility/Helper methods
-    public void OnFalling() {
+    private void OnFalling() {
         //Debug.Log("Invoke onFalling");
         isFalling = true;
         animator.SetBool("IsFalling", isFalling);
     }
 
-    public void OnLanding() {
+    private void OnLanding() {
         //Debug.Log("Invoke OnLanding");
         isJumping = false;
         isFalling = false;
@@ -157,9 +175,21 @@ public class NewPlayerMovementController : MonoBehaviour
         animator.SetBool("IsFalling", isFalling);
     }
 
-    public void IsCrouching(bool isCrouching) {
+    private void IsJumping(bool isJumping) {
+        //Debug.Log("Invoke IsJumping");
+        this.isJumping = isJumping;
+        animator.SetBool("IsJumping", isJumping);
+    }
+
+    private void IsCrouching(bool isCrouching) {
         //Debug.Log("Invoke IsCrouching");
         this.isCrouching = isCrouching;
+
+        if (playerBoxCollider2D != null) {
+            playerBoxCollider2D.enabled = !this.isCrouching;
+        }
+
+
         animator.SetBool("IsCrouching", this.isCrouching);
     }
 
